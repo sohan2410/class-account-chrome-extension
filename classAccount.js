1fb935fec9ba0ce306e725d74085fc524f0a8315
sohan2410 <bandarysohan24@gmail.com>
2021-05-22 11:43:36 +0530

    finally done

--- classAccount.js
+++ classAccount.js
@@ -1,12 +1,12 @@
 let num = 1;
 chrome.storage.sync.get("ca", function (data) {
   num = data.ca;
+  let temp = window.location.href;
+  let url = new URL(temp);
+  if (url.origin === "https://meet.google.com") {
+    alert("hello from onCreated if condition true");
+    url.search = "?authuser=" + num;
+    console.log(url.toString());
+    location.assign(url.toString());
+  }
 });
-let temp = window.location.href;
-let url = new URL(temp);
-if (url.origin === "https://meet.google.com") {
-  alert("hello from onCreated if condition true");
-  url.search = "?authuser=" + num;
-  console.log(url.toString());
-  location.assign(url.toString());
-}
